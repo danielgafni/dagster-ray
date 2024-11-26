83588c8c4be65727946a98a0f13dae1142cef991
serriaguirregaray <135058655+serriaguirregaray@users.noreply.github.com>
2024-11-26 14:02:01 -0500

    Ruff fixes

--- dagster_ray/kuberay/__init__.py
+++ dagster_ray/kuberay/__init__.py
@@ -1,9 +1,9 @@
 from dagster_ray.kuberay.configs import RayClusterConfig
 from dagster_ray.kuberay.jobs import cleanup_kuberay_clusters, delete_kuberay_clusters
 from dagster_ray.kuberay.ops import cleanup_kuberay_clusters_op, delete_kuberay_clusters_op
+from dagster_ray.kuberay.pipes import PipesKubeRayJobClient
 from dagster_ray.kuberay.resources import KubeRayCluster, RayClusterClientResource
 from dagster_ray.kuberay.schedules import cleanup_kuberay_clusters_daily
-from dagster_ray.kuberay.pipes import PipesKubeRayJobClient
 
 __all__ = [
     "KubeRayCluster",
