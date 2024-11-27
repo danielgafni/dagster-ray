dee05856c66bd83f236f4633aab285977e7ab2b6
serriaguirregaray <135058655+serriaguirregaray@users.noreply.github.com>
2024-11-27 16:40:54 -0500

    Typing and class import (#54)
    * Typing and class import
    
    * Ruff fixes
    
    ---------
    
    Co-authored-by: serriaguirregaray <sa4117@columbia.edu>

--- dagster_ray/kuberay/__init__.py
+++ dagster_ray/kuberay/__init__.py
@@ -1,6 +1,7 @@
 from dagster_ray.kuberay.configs import RayClusterConfig
 from dagster_ray.kuberay.jobs import cleanup_kuberay_clusters, delete_kuberay_clusters
 from dagster_ray.kuberay.ops import cleanup_kuberay_clusters_op, delete_kuberay_clusters_op
+from dagster_ray.kuberay.pipes import PipesKubeRayJobClient
 from dagster_ray.kuberay.resources import KubeRayCluster, RayClusterClientResource
 from dagster_ray.kuberay.schedules import cleanup_kuberay_clusters_daily
 
@@ -8,6 +9,7 @@ __all__ = [
     "KubeRayCluster",
     "RayClusterConfig",
     "RayClusterClientResource",
+    "PipesKubeRayJobClient",
     "cleanup_kuberay_clusters",
     "delete_kuberay_clusters",
     "cleanup_kuberay_clusters_op",
