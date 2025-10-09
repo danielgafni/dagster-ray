d3fa66ef59d278c72e66935cf9c9685db0222aef
Daniel Gafni <danielgafni16@gmail.com>
2025-10-09 23:06:57 +0300

    :wrench: register dagster-ray as Dagster library (#259)

--- src/dagster_ray/__init__.py
+++ src/dagster_ray/__init__.py
@@ -1,4 +1,7 @@
+from dagster._core.libraries import DagsterLibraryRegistry
+
 from dagster_ray._base.resources import RayResource
+from dagster_ray._version import __version__
 from dagster_ray.configs import Lifecycle
 from dagster_ray.core.executor import ray_executor
 from dagster_ray.core.io_manager import RayIOManager
@@ -6,6 +9,7 @@ from dagster_ray.core.pipes import PipesRayJobClient, PipesRayJobMessageReader
 from dagster_ray.core.resources import LocalRay
 from dagster_ray.core.run_launcher import RayRunLauncher
 
+DagsterLibraryRegistry.register("dagster-ray", __version__, is_dagster_package=False)
 __all__ = [
     "Lifecycle",
     "RayResource",
@@ -15,4 +19,5 @@ __all__ = [
     "PipesRayJobMessageReader",
     "PipesRayJobClient",
     "LocalRay",
+    "__version__",
 ]
